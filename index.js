1fbc3318c8afd2d4218227e014438a3abdc6ca0f
أحمد اللقماني الحربي <96401137+Ahmad-JS@users.noreply.github.com>
2022-08-04 03:08:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+//////////////////
+    const dialoG = document.getElementById("dialoG"),
+          dialogTool = document.getElementById("dialogTool"),
+          dialogHtml = ` <br>
+                      <input type="text" id="myText" placeholder="أدخل نص المهمة" required>
+                        <br>
+                      <div class="de">
+                          <button type="button" class="but2" onclick="hiddenDialog()">إلغاء
+                            </button>
+                          <button type="button"  id="add-btn" class="but2" onclick="adDtasK()">إضافة
+                            </button>
+                      </div>
+                    `;
+//////////////////
 
-                  // let tasks = [
-                  //   {
-                  //     "title": "قراءة كتاب",
-                  //     "date":"15/2/2023",
-                  //     "isDone":true
-                  //   },{
-                  //     "title": "إنهاء المشروع",
-                  //     "date":"15/12/2028",
-                  //     "isDone":true
-                  //   },{
-                  //     "title": "إنهاء الدرس",
-                  //     "date":"15/11/2027",
-                  //     "isDone":true
-                  //   },{
-                  //     "title": "حل التحدي",
-                  //     "date":"15/12/2025",
-                  //     "isDone":true
-                  //   }
-                  // ]
+          function showDialog() { 
+            dialoG.showModal(); 
+            dialogTool.innerHTML = dialogHtml;
+          } 
+          function hiddenDialog() { 
+            dialoG.close();  
+          } 
+          function showDialog2() { 
+            dialoG.showModal(); 
+          } 
 
           function getTaskFromStorage(){
             let retrievedTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -25,8 +30,9 @@
           }
           getTaskFromStorage()
 
+          const taskS = document.getElementById("tasks");
           function fillTasksOnThePage(){
-            document.getElementById("tasks").innerHTML="";
+            taskS.innerHTML="";
             let index = 0,
             calendaR_title = "تاريخ إضافة المهمة",
             deletE_title = "حذف المهمة";
@@ -37,88 +43,183 @@
               let content = 
               `
           <div id="task" class="task ${task.isDone ? 'done':''}">
-                <div style="width: 60%;">
-                    <h3>${task.title}</h3>
-                    <div title="${calendaR_title}">
+                <div class="task-text">
+                <h3>${index+1} - ${task.title}</h3>
+                    <div class="calendar" title="${calendaR_title}">
                         <span class="material-symbols-outlined calendar text-size">
                         calendar_month
                         </span>
-                        <span class="text-size">
+                        <span class="date-size">
                         ${task.date}
                         </span>
                     </div>
                 </div>
           <div class="group-buttons">
                   <button onclick="deleteTask(${index})" class="circular red" title="${deletE_title}">
-                      <span class="material-symbols-outlined">
+                      <span class="material-symbols-outlined icon-size">
                       delete
                       </span>
                   </button>
             ${task.isDone ?`
                   <button onclick="toggleTaskCompletion(${index})" class="circular darkgreen" title="${removE_title}">
-                    <span class="material-symbols-outlined">
+                    <span class="material-symbols-outlined icon-size">
                       remove_done
                     </span>
                   </button>
             `:`
                   <button onclick="toggleTaskCompletion(${index})" class="circular green" title="${donE_title}">
-                      <span class="material-symbols-outlined">
+                      <span class="material-symbols-outlined icon-size">
                       done
                       </span>
                   </button>
             
             `}
             <button onclick="editTask(${index})" class="circular blue" title="${ediT_title}">
-              <span class="material-symbols-outlined">
+              <span class="material-symbols-outlined icon-size">
                 edit
                 </span>
             </button>
           </div>
         </div>
               `
-          document.getElementById("tasks").innerHTML +=content;
-          index++
+              taskS.innerHTML +=content;
+              index++
             }
       }
-          fillTasksOnThePage();
-          document.getElementById("add-btn").addEventListener("click",()=>{
-            let now = new Date(),
-            date = now.getFullYear() + "/" + (now.getMonth()+1) + "/" + now.getDay() + " &#128338; " + now.getMinutes() + " : " + now.getHours(),
-            taskName = prompt("الرجاء إدخال عنوان المهمة");
-          let  taskObj = {
-                      "title": taskName,
-                      "date":date,
-                      "isDone":false
-                          }
-                tasks.push(taskObj);
+
+                    fillTasksOnThePage();
+                    function adDtasK(){
+                      let datE = new Date(),
+                      date =  datE.toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })
+                      var myText = document.getElementById("myText");
+                      let taskName =   myText.value;
+                      if (taskName===""){
+                        let noText = dialogTool.innerHTML =
+                        `
+                        <p style="font-size: small;color: red;">حقل الإدخال يجب أن لايكون فارغ . !</p>
+                            <input type="text" id="myText" placeholder="أدخل نص المهمة" required>
+                            <div class="de">
+                    <button type="button" class="but2" onclick="hiddenDialog()">إلغاء
+                      </button>
+                    <button type="button"  id="add-btn" class="but2" onclick="adDtasK()">إضافة
+                      </button>
+                </div>
+                        `
+                        ;
+                      return noText;
+
+                      }
+                    let  taskObj = {
+                                "title": taskName,
+                                "date":date,
+                                "isDone":false
+                                    }
+                          tasks.unshift(taskObj);
+                          storeTasks();
+                          fillTasksOnThePage();
+                          hiddenDialog();
+                          toasT('blue');
+          }
+
+            function deleteTask(index){
+              showDialog2();
+                    let task = tasks[index];
+                    dialogTool.innerHTML =
+                        ` <p style="font-size: small;color: red;">هل أنت متأكد من حذف :</p>
+                            <p>${index+1} - ${task.title}</p> 
+                <div class="de">
+                <button id="nodelet" class="but2" onclick="hiddenDialog()">لا</button>
+                  <button id="delet" class="but2" onclick="isConfirm(${index})">نعم</button>
+                </div>
+                        `;
+                }
+              
+            function isConfirm(index) {
+                tasks.splice(index,1);
                 storeTasks();
                 fillTasksOnThePage();
-          });
-
-          function deleteTask(index){
-            let task = tasks[index],
-                isConfirmed = confirm("هل أنت متأكد من احذف :"+task.title);
-            if(isConfirmed)
-              tasks.splice(index,1);
-              storeTasks();
-              fillTasksOnThePage();
-          }
-          function editTask(index){
-                let task = tasks[index],
-                newTaskTitle = prompt("الرجاء إدخال عنوان المهمة الجديد",task.title);
-
-                task.title = newTaskTitle;
-                storeTasks();
-                fillTasksOnThePage();
-          }
-          function toggleTaskCompletion(index){
+                hiddenDialog();
+                toasT('red');
+            }
+        
+              function editTask(index){
+                showDialog2();
                 let task = tasks[index];
-                task.isDone = !task.isDone;
+                dialogTool.innerHTML =
+                ` <p>إدخال عنوان المهمة الجديد</p>
+                    
+                    <input type="text" id="editText" value="${task.title}" required>
+                    <div class="de">
+                    <br>
+                    <br>
+                    <button id="nodelet" class="but2" onclick="hiddenDialog()">إلغاء</button>
+                    <button id="delet" class="but2" onclick="editTask2(${index})">تحديث</button>
+                  </div>
+                `
+                ;
+            }
+
+          function editTask2(index) {
+                let task = tasks[index];
+                var editText = document.getElementById("editText");
+                let taskName =   editText.value;
+                if (taskName===""){
+                  let noText = dialogTool.innerHTML =
+                  `
+                  <p style="font-size: small;color: #ff1500;">حقل الإدخال يجب أن لايكون فارغ . !</p>
+                      <input style="font-size: small;color: #ff1500;" type="text" id="myText" placeholder="تعديل او تحديث فقط" required><div class="de">
+                      <br>
+                      <br>
+                      <button id="nodelet" class="but2" onclick="toasT('orange')">إلغاء العملية</button>
+                      
+                    </div>
+                      
+                  `
+                  ;
+                return noText;
+
+                }
+                task.title = taskName;
                 storeTasks();
                 fillTasksOnThePage();
-          }
-          function storeTasks(){
-            let tasksString =JSON.stringify(tasks);
-            localStorage.setItem("tasks",tasksString);
-          }
+                hiddenDialog();
+                toasT('turquoise');
+            }
+              function toggleTaskCompletion(index){
+                    let task = tasks[index];
+                    task.isDone = !task.isDone;
+                    storeTasks();
+                    fillTasksOnThePage();
+                    console.log(task.isDone)
+                    if(!task.isDone)
+                    toasT('wheat');
+                    else toasT('green');
+              }
+              function storeTasks(){
+                let tasksString =JSON.stringify(tasks);
+                localStorage.setItem("tasks",tasksString);
+              }
+
+////////
+              function toasT(msgColor) {
+                
+                if(msgColor==='green')
+                  msgText ="رائع..مهمة منجزة !";
+                if(msgColor==='wheat')
+                    msgText ="مهمة تحتاج إكمال..!";
+                if(msgColor==='red')
+                  msgText ="تمت عملية الحذف !";
+                if(msgColor==='blue')
+                  msgText ="تمت الاضافة بنجاح !";
+                if(msgColor==='orange')
+                  msgText ="تم الغاء العملية !",
+                  hiddenDialog();
+                if(msgColor==='turquoise')
+                  msgText ="تم تحديث المهمة بنجاح !";
+                let toastText = document.getElementById("toastText");
+                toastText.innerHTML=`<p class="toastText ${msgColor}">${msgText}</p>`;
+                toastText.className = "show";
+                setTimeout(()=>  toastText.className = toastText.className.replace("show",""),3000);
+              }
+
 
